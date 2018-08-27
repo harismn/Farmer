889d9f4fb3eecea2c33c703c42780a318db07769
riaanggraini99 <ria.anggraini@students.amikom.ac.id>
2018-08-27 14:11:49 +0700

    small fix

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/user', user);
 app.use('/article', article);
 // Setup a default catch-all route that sends back a welcome message in JSON format.
